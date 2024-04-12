52e96c8c4ddc056d5404cfdd8478324f1ce8b0cc
beratridvanasilturk <br.asltrk@gmail.com>
2024-04-12 20:35:43 +0300

    Updated MVVM Arc.

--- InvioCekirgeProgrami2024/InvioCekirgeProgrami2024/MainTableView/ViewController/MainTableViewController.swift
+++ InvioCekirgeProgrami2024/InvioCekirgeProgrami2024/MainTableView/ViewController/MainTableViewController.swift
@@ -4,131 +4,84 @@
 //
 //  Created by Berat Ridvan Asilturk on 3.04.2024.
 //
-
-// TODO: - Classlar final
-// Parametleler ve methodlar private // encapsulation
-// MVVM cevir
+// TODO: -
 // OPTIONAL KURTUL
-// KLASORLE
 // Favoriler Realm
-// Printler kaldirilabilir proje tesliminde
-// DidLoad Refactor Edilecek
 
 import UIKit
 
 final class MainTableViewController: UITableViewController {
     
     private let cellIdentifier = "cellIdentifier"
-    private var dataArray = [DataResponseModel]()
-    private var totalPage = 1
-    private var currentPage = 1
-    private var refresh = false
+    private let viewModel: MainViewModel
+    
+    required init?(coder: NSCoder) {
+        self.viewModel = MainViewModel()
+        super.init(coder: coder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loadDatas()
-        
+        getData()
     }
     
-    private func fetchData(pageNum: Int) {
+    private func getData() {
+        viewModel.fetchData(pageNum: viewModel.currentPage, completion: {
+            self.setUI()
+        })
+    }
+    
+    private func setUI() {
         
-        checkRefreshing()
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl?.addTarget(self, action: #selector(self.refreshData), for: .valueChanged)
         
-        let baseUrlString = "https://storage.googleapis.com/invio-com/usg-challenge/universities-at-turkey/"
-        let urlString = baseUrlString + "page-\(pageNum).json"
-        
-        guard let url = URL(string: urlString) else {
-            print("😵😵😵 URL ERROR")
-            return
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            print("✅✅✅ UI UPDATED")
         }
+    }
+    
+    @objc private func refreshData() {
         
-        let session = URLSession.shared
-        let dataTask = session.dataTask(with: url) { data, response, error in
-            
-            guard error == nil else {
-                print(error?.localizedDescription)
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            
-            do {
-                let responseModel = try decoder.decode(MainResponseModel.self, from: data)
-                self.totalPage = responseModel.totalPage ?? 1
-                if let parsingData = responseModel.data {
-                    self.dataArray.append(contentsOf: parsingData)
-                }
-                
-                print("Cities Count = \(self.dataArray.count)")
-               
-                reloadData()
-                
-            } catch(let err) {
-                print("⭕️⭕️⭕️ PARSING ERROR \(err.localizedDescription)")
-            }
-        }
-        dataTask.resume()
+        viewModel.currentPage = 1
         
-        func checkRefreshing() {
-            DispatchQueue.main.async {
-                if self.refresh {
-                    self.refreshControl?.beginRefreshing()
-                    self.dataArray.removeAll()
-                    self.refreshControl?.endRefreshing()
-                    self.refresh.toggle()
-                    print("🔄🔄🔄 Refreshed")
-                }
-            }
-        }
+        self.refreshControl?.beginRefreshing()
+        self.viewModel.dataArray.removeAll()
         
-        func reloadData() {
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
+        viewModel.fetchData(pageNum: viewModel.currentPage, completion: {
+            self.tableView.reloadData()
+        })
+        
+        self.refreshControl?.endRefreshing()
+        print("🔄🔄🔄 Refreshed")
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        return viewModel.dataArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if currentPage < totalPage && indexPath.row == dataArray.count - 1 {
+        if viewModel.currentPage < viewModel.totalPage && indexPath.row == viewModel.dataArray.count - 1 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "loading")
             return cell!
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-            let data = dataArray[indexPath.row]
+            let data = viewModel.dataArray[indexPath.row]
             cell.textLabel?.text = data.province
             return cell
         }
     }
     
-    // Pagination / Sayfalama
+    // Pagination
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if currentPage < totalPage && indexPath.row == dataArray.count - 1 {
-            currentPage = currentPage + 1
-            fetchData(pageNum: currentPage)
+        if viewModel.currentPage < viewModel.totalPage && indexPath.row == viewModel.dataArray.count - 1 {
+            viewModel.currentPage = viewModel.currentPage + 1
+            
+            viewModel.fetchData(pageNum: viewModel.currentPage, completion: {
+                self.setUI()
+            })
         }
     }
-    
-    private func loadDatas() {
-
-        fetchData(pageNum: currentPage)
-        self.refreshControl = UIRefreshControl()
-        self.refreshControl?.addTarget(self, action: #selector(refreshData), for: .valueChanged)
-    }
-    
-    @objc private func refreshData() {
-        currentPage = 1
-        refresh = true
-        fetchData(pageNum: currentPage)
-    }
-    
 }
--- InvioCekirgeProgrami2024/InvioCekirgeProgrami2024/MainTableView/ViewModel/MainViewModel.swift
+++ InvioCekirgeProgrami2024/InvioCekirgeProgrami2024/MainTableView/ViewModel/MainViewModel.swift
@@ -7,8 +7,51 @@
 
 import Foundation
 
-class MainViewModel {
+final class MainViewModel {
     
+    var dataArray = [DataResponseModel]()
+    var totalPage = 1
+    var currentPage = 1 
     
-    
+    func fetchData(pageNum: Int, completion: (() -> Void)? = nil) {
+        
+        let baseUrlString = "https://storage.googleapis.com/invio-com/usg-challenge/universities-at-turkey/"
+        let urlString = baseUrlString + "page-\(pageNum).json"
+        
+        guard let url = URL(string: urlString) else {
+            print("😵😵😵 URL ERROR")
+            return
+        }
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            
+            guard error == nil else {
+                print(error?.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let responseModel = try decoder.decode(MainResponseModel.self, from: data)
+                self.totalPage = responseModel.totalPage ?? 1
+                if let parsingData = responseModel.data {
+                    self.dataArray.append(contentsOf: parsingData)
+                }
+                
+                print("🟡🟡🟡 Cities Count = \(self.dataArray.count)")
+               
+                completion?()
+                
+            } catch(let err) {
+                print("⭕️⭕️⭕️ PARSING ERROR \(err.localizedDescription)")
+            }
+        }
+        dataTask.resume()
+    }
 }

f71127a339a6d48c378d8dc668492ca524dd2082
beratridvanasilturk <br.asltrk@gmail.com>
2024-04-18 05:29:30 +0300

    Added CheckHttps and CheckPhone Funcs && Added TODO List

---
+++ InvioCekirgeProgrami2024/InvioCekirgeProgrami2024/TODO/TodoList.swift
@@ -0,0 +1,14 @@
+//
+//  TodoList.swift
+//  InvioCekirgeProgrami2024
+//
+//  Created by Berat Ridvan Asilturk on 18.04.2024.
+//
+
+// TODO:
+// - Telefon Numaralari ve Website'leri detayli test edilecek. App Bug'a dusmemek icin olasi buglar fixlenecek
+// -
+// - Tanitim Screen Records Alinip Eklenecek
+// -
+// - Add MARK Props DidLoad (LifeCycle) vs
+// - Need Fix UI Update Delegate Issue When Tapped UnFav
